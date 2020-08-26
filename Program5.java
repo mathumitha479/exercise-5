cea23b288e7e1990e9a4373c9dec57fa349d77b6
mathumitha479 <68961998+mathumitha479@users.noreply.github.com>
2020-08-26 19:37:31 +0530

    Add files via upload

---
+++ Program5.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program5;
+
+/**
+ *
+ * @author MADUMITHA
+ */
+public class Program5 {
+
+    /**
+     * @param args the command line arguments
+     */
+ 
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+        UG obj1=new UG();
+        obj1.getdegree();
+        PG obj=new PG();
+        obj.getdegree();
+
+        
+    }
+    
+}
+class degree
+{
+    void getdegree()
+    {
+        System.out.println("i got a degree");
+    }
+}
+class UG extends degree 
+{
+     
+    void getdegree()
+    {   
+        super.getdegree();
+        System.out.println("I am undergraduate");
+    }
+}
+class PG extends UG
+{
+   
+    void getdegree()
+    {
+        System.out.println("I am postgraduate");
+    }
+}
+
+
